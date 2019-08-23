111a7730cd4e5607daf89a3d78adbf8c275beda2
Wouter van Heeswijk <wouter.vanheeswijk@tomtom.com>
2019-08-23 15:26:53 +0200

    Initial version 2 commit

---
+++ src/floaty.ts
@@ -0,0 +1,11 @@
+import { State } from './state';
+
+export class Floaty {
+    private currentState: State;
+
+    public constructor(initialState: State) {
+        this.currentState = initialState;
+    }
+
+    
+}
--- src/identifiers.tsx
+++
@@ -1,5 +0,0 @@
-import * as uuid from 'uuid';
-
-export function generateIdentifier(): string {
-    return uuid.v4() as string;
-}

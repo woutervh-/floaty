62aeb1f3e89d0afb9441960bd9cdd3d2a3db0308
Wouter van Heeswijk <wouter.vanheeswijk@tomtom.com>
2019-09-26 12:43:27 +0200

    Render floating tab above floating drop area

--- src/renderers/floating-renderer.tsx
+++ src/renderers/floating-renderer.tsx
@@ -4,8 +4,8 @@ import * as RenderersModel from '../renderers-model';
 export class FloatingRenderer<T> extends React.PureComponent<RenderersModel.FloatingRendererProps<T>, never> {
     public render() {
         return <div style={{ display: 'grid', gridTemplateRows: 'minmax(min-content, max-content) 1fr' }}>
-            <this.props.floatyRenderers.floatingTabRenderer floatyManager={this.props.floatyManager} stackItem={this.props.floating} />
             <this.props.floatyRenderers.floatingContentRenderer floatyManager={this.props.floatyManager} stackItem={this.props.floating} />
+            <this.props.floatyRenderers.floatingTabRenderer floatyManager={this.props.floatyManager} stackItem={this.props.floating} />
         </div>;
     }
 }

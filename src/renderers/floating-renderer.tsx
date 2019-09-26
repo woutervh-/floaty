1527dec1655120cf52796dd0b2ada6db243a55f6
Wouter van Heeswijk <wouter.vanheeswijk@tomtom.com>
2019-09-26 12:39:53 +0200

    Allow floating tab to be any size

--- src/renderers/floating-renderer.tsx
+++ src/renderers/floating-renderer.tsx
@@ -3,7 +3,7 @@ import * as RenderersModel from '../renderers-model';
 
 export class FloatingRenderer<T> extends React.PureComponent<RenderersModel.FloatingRendererProps<T>, never> {
     public render() {
-        return <div style={{ display: 'grid', gridTemplateRows: '20px 1fr' }}>
+        return <div style={{ display: 'grid', gridTemplateRows: 'minmax(min-content, max-content) 1fr' }}>
             <this.props.floatyRenderers.floatingTabRenderer floatyManager={this.props.floatyManager} stackItem={this.props.floating} />
             <this.props.floatyRenderers.floatingContentRenderer floatyManager={this.props.floatyManager} stackItem={this.props.floating} />
         </div>;
